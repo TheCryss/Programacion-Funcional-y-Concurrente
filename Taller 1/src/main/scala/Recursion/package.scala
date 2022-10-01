0442acecca5e65198cbe6373adc4fafa539d8455
Bucheli <jose.bucheli@correounivalle.edu.co>
2022-10-01 17:42:09 -0500

    mcd por Algoritmo de la Division

--- Taller 1/src/main/scala/Recursion/package.scala
+++ Taller 1/src/main/scala/Recursion/package.scala
@@ -1,4 +1,5 @@
 package object Recursion {
+  //Calcular el minimo comun divisor mediante el TFA
   def mcdTFA(ln: List[Int], lm: List[Int], primos: List[Int]): Double = {
     def cmpEXPO(ln: List[Int],lm: List[Int]):Int = if (ln.head>lm.head) lm.head else ln.head
     def pwrPrime(primes: List[Int], ln: List[Int], lm: List[Int]): Double = scala.math.pow(primes.head,cmpEXPO(ln, lm))
@@ -8,5 +9,17 @@ package object Recursion {
     }
     pwrs(primos, ln, lm,1)
   }
+  //Calcular el minimo comun divisor y coneficientes de berzuit mediante el algoritmo de Euclides
+  def mcdEB(n: Int, m: Int): Int = {
+    def q(n: Int, m: Int, result: Int): Int = if (n > m) q(n - m, m, result + 1) else return result
+    def divs(n: Int, m: Int, r: Int): Int = {
+      if (r == 0) m
+      else
+        divs(m, n - (m* q(n, m, 0)), (n-m)*q(n, m, 0))
+    }
+
+    divs(n, m, 1)
+  }
+ // def div(n:Int,m:Int,q:Int):Int= if (n>m) div(n-m,m, q+1) else return q
 }
 
--- Taller 1/src/test/scala/Pruebas.sc
+++ Taller 1/src/test/scala/Pruebas.sc
@@ -1,2 +1,12 @@
 import Recursion._
-mcdTFA(List(3,1,1),List(2,0,3),List(2,3,5))
+//Teorema Fundamental de la Aritmetica
+
+mcdTFA(List(3,1,1),List(2,0,3),List(2,3,5))//120 y 500
+mcdTFA(List(0,0,1),List(1,1,0),List(2,3,5))//5 y 6 (Primos relativos)
+mcdTFA(List(0,3,0,0,0,0,0,0,0,0,0,0,0,0,0),List(0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),List(2,3,5,7,11,13,17,19,23,29,31,37,41,43,47))//27 y 43
+mcdTFA(List(1,0,2),List(1,0,2),List(2,3,5))//50 y 50 (Numeros iguales)
+mcdTFA(List(1,0,0),List(0,1,0),List(2,3,5))//Primos relativos2
+//Algoritmo de la Division
+
+mcdEB(963,657)
+mcdEB(500,120)
